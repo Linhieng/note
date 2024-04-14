b1bbc034cbcc4c4f1dc4629a0471f22d6f71388f
linhieng <790728740@qq.com>
2024-04-14 15:59:31 +0800

    cd: update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,30 +3,38 @@ import fs from 'node:fs'
 import { join, extname, basename } from 'node:path'
 
 const nav = [
-    { pathPrefix: '/web/', dir: 'web/', text: 'web 相关', link: '/web/README' },
-    { pathPrefix: '/vscode/', dir: 'vscode/', text: 'vscode', link: '/vscode/README' },
+    // { pathPrefix: '/web/', dir: 'web/', text: 'web 相关', link: '/web/README' },
+    // { pathPrefix: '/vscode/', dir: 'vscode/', text: 'vscode', link: '/vscode/README' },
     { pathPrefix: '/DSA/', dir: 'DSA/', text: '数据结构与算法', link: '/DSA/README' },
-    { pathPrefix: '/CLI/', dir: 'CLI/', text: '命令行', link: '/CLI/README' },
-    { pathPrefix: '/interview/', dir: 'interview/', text: '面试相关', link: '/interview/css' },
+    // { pathPrefix: '/CLI/', dir: 'CLI/', text: '命令行', link: '/CLI/README' },
+    // { pathPrefix: '/interview/', dir: 'interview/', text: '面试相关', link: '/interview/css' },
 ]
 const getNav = () => nav.map(item => ({ text: item.text, link: item.link }))
 
 function dfsReadme(dir) {
     const ans = []
-    for (const child of fs.readdirSync(dir)) {
+    const subFiles = fs.readdirSync(dir)
+    subFiles.sort((a,b) => {
+        // 我要让以 _ 开头的排在前面
+        if (a.startsWith('_') && !b.startsWith('_')) return -1
+        else if (!a.startsWith('_') && b.startsWith('_')) return 1
+        return a-b
+    })
+
+    for (const child of subFiles) {
         const childPath = join(dir, child)
         if (isDir(childPath)) {
             const items = dfsReadme(childPath)
             if (items.length > 0) {
                 ans.push({
-                    text: basename(childPath),
+                    text: basename(childPath, '.md'),
                     items
                 })
 
             }
         } else if (extname(childPath) === '.md') {
             ans.push({
-                text: basename(childPath),
+                text: basename(childPath, '.md'),
                 link: childPath
             })
         }
@@ -38,6 +46,7 @@ function dfsReadme(dir) {
  */
 function getOneSidebar(pathPrefix) {
     const sidebar = dfsReadme(pathPrefix)
+    console.log(sidebar);
     return sidebar
         .filter(v => !(v.text === 'README.md'))
         .map(v => ({ ...v, collapsed: true }))
@@ -64,7 +73,7 @@ export default defineConfig({
 
         editLink: {
             text: '在 Github 上查看此页',
-            pattern: 'https://github.com/linhieng/note/main/:path'
+            pattern: 'https://github.com/linhieng/note/tree/main/:path'
         },
         socialLinks: [
             { icon: 'github', link: 'https://github.com/linhieng/note' }
